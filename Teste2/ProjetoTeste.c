f8a3677c7487620c7907b1ae3d07150e6425de4a
ErickLima <erickcreimberg@gmail.com>
2022-10-03 17:51:28 -0300

    Adicionado a movimentacao com FPS

--- Teste2/ProjetoTeste.c
+++ Teste2/ProjetoTeste.c
@@ -279,12 +279,125 @@ void Timing()
 	al_destroy_display(display);
 }
 
+void InputTimer()
+{
+	bool done = false;
+	bool redraw = true;
+
+	int pos_x = width / 2;
+	int pos_y = height / 2;
+	
+	int FPS = 60;
+
+	bool keys[4] = { false, false, false, false };
+
+	if (!al_init())
+		return -1;
+
+
+	display = al_create_display(width, height);
+
+	if (!display)
+		return -1;
+
+	al_init_primitives_addon();
+	al_install_keyboard();
+
+	event_queue = al_create_event_queue();
+	timer = al_create_timer(1.0 / FPS);
+
+	al_register_event_source(event_queue, al_get_keyboard_event_source());
+	al_register_event_source(event_queue, al_get_display_event_source(display));
+	al_register_event_source(event_queue, al_get_timer_event_source(timer));
+
+	al_start_timer(timer);
+
+	while (!done)
+	{
+		ALLEGRO_EVENT ev;
+		al_wait_for_event(event_queue, &ev);
+
+		if (ev.type == ALLEGRO_EVENT_KEY_DOWN)
+		{
+			switch (ev.keyboard.keycode)
+			{
+			case ALLEGRO_KEY_UP:
+			case ALLEGRO_KEY_W:
+				keys[UP] = true;
+				break;
+			case ALLEGRO_KEY_DOWN:
+			case ALLEGRO_KEY_S:
+				keys[DOWN] = true;
+				break;
+			case ALLEGRO_KEY_RIGHT:
+			case ALLEGRO_KEY_D:
+				keys[RIGHT] = true;
+				break;
+			case ALLEGRO_KEY_LEFT:
+			case ALLEGRO_KEY_A:
+				keys[LEFT] = true;
+				break;
+			}
+		}
+		else if (ev.type == ALLEGRO_EVENT_KEY_UP)
+		{
+			switch (ev.keyboard.keycode)
+			{
+			case ALLEGRO_KEY_UP:
+			case ALLEGRO_KEY_W:
+				keys[UP] = false;
+				break;
+			case ALLEGRO_KEY_DOWN:
+			case ALLEGRO_KEY_S:
+				keys[DOWN] = false;
+				break;
+			case ALLEGRO_KEY_RIGHT:
+			case ALLEGRO_KEY_D:
+				keys[RIGHT] = false;
+				break;
+			case ALLEGRO_KEY_LEFT:
+			case ALLEGRO_KEY_A:
+				keys[LEFT] = false;
+				break;
+			case ALLEGRO_KEY_ESCAPE:
+				done = true;
+				break;
+			}
+		}
+		else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+		{
+			done = true;
+		}
+		else if (ev.type == ALLEGRO_EVENT_TIMER)
+		{
+			pos_y -= keys[UP] * 10;
+			pos_y += keys[DOWN] * 10;
+			pos_x -= keys[LEFT] * 10;
+			pos_x += keys[RIGHT] * 10;
+
+			redraw = true;
+		}
+
+		if (redraw && al_is_event_queue_empty(event_queue))
+		{
+			redraw = false;
+
+			al_draw_filled_rectangle(pos_x, pos_y, pos_x + 30, pos_y + 30, al_map_rgb(0, 0, 255));
+			al_flip_display();
+			al_clear_to_color(al_map_rgb(0, 0, 0));
+		}
+
+	}
+
+	al_destroy_display(display);
+}
+
 
 int main(void)
 {
 	int item;
 	do {
-		printf("Digite o que voce deseja ver:\n\n0: Sair\n1: Operação Teste\n2: Construindo Geometrias\n3: Input Keyboard\n4: Mouse Input\n5: Timer\n\nEscolha: ");
+		printf("Digite o que voce deseja ver:\n\n0: Sair\n1: Operação Teste\n2: Construindo Geometrias\n3: Input Keyboard\n4: Mouse Input\n5: Timer\n6: InputTimer\n\nEscolha: ");
 
 		scanf_s("%d", &item);
 
@@ -307,6 +420,9 @@ int main(void)
 			break;
 		case 5:
 			Timing();
+			break;
+		case 6:
+			InputTimer();
 			break;
 		default:
 			printf("Não foram digitados números correspondentes, digite novamente!");
