c9debf9c4946a9e83aa790803c6cbde545a2d6be
GUSTAVO PAGNOTTA FARIA <gustavo.pfaria1@senacsp.edu.br>
2022-10-27 18:58:43 -0300

    Arrumado o código de inicialização.

--- Main/Init.c
+++ Main/Init.c
@@ -3,33 +3,33 @@
 Allegro init() {
 	Allegro allegro;
 
-	if (!al_init()) {
-		allegro.deuCerto = false;
-		return allegro;
-	}
+	if (al_init()) {
 
-	allegro.display = al_create_display(WIDTH, HEIGHT);
-	allegro.eventQueue = al_create_event_queue();
-	allegro.timer = al_create_timer(1.0 / FPS);
+		allegro.display = al_create_display(WIDTH, HEIGHT);
+		allegro.eventQueue = al_create_event_queue();
+		allegro.timer = al_create_timer(1.0 / FPS);
 
-	if (deuCerto(&allegro)) {
-		allegro.deuCerto = true;
+		if (deuCerto(&allegro)) {
+			allegro.deuCerto = true;
 
-		al_install_keyboard();
-		al_init_image_addon();
+			al_install_keyboard();
+			al_init_image_addon();
 
-		al_register_event_source(allegro.eventQueue, al_get_keyboard_event_source());
-		al_register_event_source(allegro.eventQueue, al_get_display_event_source(allegro.display));
-		al_register_event_source(allegro.eventQueue, al_get_timer_event_source(allegro.timer));
+			al_register_event_source(allegro.eventQueue, al_get_keyboard_event_source());
+			al_register_event_source(allegro.eventQueue, al_get_display_event_source(allegro.display));
+			al_register_event_source(allegro.eventQueue, al_get_timer_event_source(allegro.timer));
 
-		al_start_timer(allegro.timer);
-
-		return allegro;
+			al_start_timer(allegro.timer);
+		}
+		else {
+			allegro.deuCerto = false;
+		}
 	}
 	else {
 		allegro.deuCerto = false;
-		return allegro;
 	}
+
+	return allegro;
 }
 bool deuCerto(Allegro* allegro) {
 	return allegro->display != NULL && allegro->eventQueue != NULL && allegro->timer != NULL;
